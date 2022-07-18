47d027a44f4c1c222a4808244498a9fbf8c4e62d
Hung Vo <vohungbk@gmail.com>
2022-07-18 14:39:59 +0700

    Add GA

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,27 @@ import type { AppProps } from 'next/app'
 import Head from 'next/head'
 import { LazyMotion, domAnimation } from 'framer-motion'
 import 'locomotive-scroll/dist/locomotive-scroll.css'
+import { useEffect } from 'react'
 
 import '../styles/globals.css'
+import Script from 'next/script'
+import * as gtag from '../lib/gtag'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url: string) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       <Head>
@@ -24,6 +41,24 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta property="twitter:image" content="/images/avatar.png" />
       </Head>
       <LazyMotion features={domAnimation}>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
+        />
+        <Script
+          id="gtag-init"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <Component {...pageProps} />
       </LazyMotion>
     </>
